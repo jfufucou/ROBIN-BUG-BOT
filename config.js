76a34e4d21b77b1e353bb23776a5f6b3ad2ce373
jfufucou <jfufucou@users.noreply.github.com>
2026-08-11 18:46:22 +0000

    Initial commit: Remove channel/group restrictions

---
+++ config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    mainToken: '8380669380:AAE2DEgbUsBg6kjCptD3GV-K_h9nAm3K8OE', // Bot Token Here 
+    
+    S7: 'ALI HERE BUG', // Owner, Name
+    
+    adminId: '8897784616', // Owner Chat ID ( control /address, /delresell )
+    
+    bot: 'KING ROBIN BUG', // Bot name 
+    
+    logo: './SY/Loves.jpg' // Bot logo path or URL
+};
